9e10cba2f46d8af2b62788d8b08f38d1bade675d
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2025-08-12 23:57:18 +0300

    IV-build3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL: 'http://rsw0k8c84ckgss00wckwkso8.148.230.125.92.sslip.io/api/',
+  baseURL: 'http://rsw0k8c84ckgss00wckwkso8.148.230.125.92.sslip.io/api',
 };
